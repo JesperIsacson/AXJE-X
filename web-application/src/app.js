a54827a820dc918769f3539149348ccfc4447a9d
Jesper Isacson <tehjaspar@outlook.com>
2020-01-29 16:01:02 +0100

    Docker setup complete

---
+++ web-application/src/app.js
@@ -0,0 +1,17 @@
+//docker build --tag=my-web-app:latest .
+//docker stop <container id>   (docker container ls)
+//docker run -p 3000:8080 my-web-app:latest
+
+
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(request, response){
+    response.send("Hellor world!")
+})
+
+app.listen(8080, function(){
+    console.log("App listening on port 8080")
+})
