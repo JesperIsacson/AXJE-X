9fc3cba72f99b803a4c7d777fac2e406306b0ae8
Jesper Isacson <tehjaspar@outlook.com>
2020-02-03 10:42:18 +0100

    docker comments

--- web-application/src/app.js
+++ web-application/src/app.js
@@ -2,6 +2,8 @@
 //docker stop <container id>   (docker container ls)
 //docker run -p 3000:8080 my-web-app:latest
 
+//docker-compose up --build      (Använd vid start eller vid ändring i en image)
+//docker-compose up     (Kör den senaste builden på imagen)
 
 
 const express = require('express')
