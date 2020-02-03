af3fc9904ad276613f4868c1527a727caf6e66e1
Jesper Isacson <tehjaspar@outlook.com>
2020-02-03 14:38:02 +0100

    folders and files created

--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -5,13 +5,19 @@
 //docker-compose up --build      (Använd vid start eller vid ändring i en image)
 //docker-compose up     (Kör den senaste builden på imagen)
 
-
+const expressHandlebars = require('express-handlebars')
 const express = require('express')
 
 const app = express()
 
+app.set("views", "src/pl/views")
+
+app.engine("hbs", expressHandlebars({
+    defaultLayout: "main.hbs"
+}))
+
 app.get('/', function(request, response){
-    response.send("Hellor world!")
+    response.render("hombe.hbs")
 })
 
 app.listen(8080, function(){
