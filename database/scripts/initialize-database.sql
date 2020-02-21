e8777e50cd6c5ce36c6ae4d8ea7ee7df244f69cc
Jesper Isacson <tehjaspar@outlook.com>
2020-02-21 11:07:58 +0100

    changed tables

--- database/scripts/initialize-database.sql
+++ database/scripts/initialize-database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Users(
+CREATE TABLE IF NOT EXISTS Users(
     _email VARCHAR(100) PRIMARY KEY NOT NULL,
     _username VARCHAR(100) UNIQUE NOT NULL,
     _firstName VARCHAR(100) NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE Users(
     _height FLOAT
 );
 
-CREATE TABLE Activities(
+CREATE TABLE IF NOT EXISTS Activities(
     _id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     _activityName TEXT NOT NULL,
     _activityDate DATE NOT NULL,
     _activityTIme TIME NOT NULL
 );
 
-CREATE TABLE Locations(
+CREATE TABLE IF NOT EXISTS Locations(
     _id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     _locationName VARCHAR(100) NOT NULL
 );
