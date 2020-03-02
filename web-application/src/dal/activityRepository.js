610a2164429bbd980cbb6a0f20463fd0eb035357
Axel Dalbard <daax18mk@student.ju.se>
2020-03-02 16:58:05 +0100

    Fixed the CRUD operations 'update' and 'delete' in activities

--- web-application/src/dal/activityRepository.js
+++ web-application/src/dal/activityRepository.js
@@ -21,11 +21,30 @@ exports.getActivityById = function(id, callback){
 
 exports.createActivity = function(activity, callback){
     
-    const query = "INSERT INTO Activities (_activityName, _activityDate, _activityTime, _activityLocation, _activityDescription) VALUES (?, ?, ?, ?, ?)"
-    const values = [activity.title, activity.date, activity.time, activity.location, activity.description]
+    const query = "INSERT INTO Activities (_activityName, _activityDate, _activityTime, _activityLocation, _activityDescription, _datePosted) VALUES (?, ?, ?, ?, ?, ?)"
+    const values = [activity.title, activity.date, activity.time, activity.location, activity.description, activity.datePosted]
 
     db.query(query, values, function(error){
         const id = this.lastID
         callback(error, id)
+    })
+}
+
+exports.updateActivity = function(activity, callback){
+
+    const query = "UPDATE Activities SET _activityName = ?, _activityDate = ?, _activityTime = ?, _activityLocation = ?, _activityDescription = ? WHERE _id = ?"
+    const values = [activity.title, activity.date, activity.time, activity.location, activity.description, activity.id]
+
+    db.query(query, values, function(error){
+        callback(error)
+    })
+}
+
+exports.deleteActivity = function(id, callback){
+
+    const query = "DELETE FROM Activities WHERE _id = ?"
+
+    db.query(query, [id], function(error){
+        callback(error)
     })
 }
--- web-application/src/pl/routers/activityRouter.js
+++ web-application/src/pl/routers/activityRouter.js
@@ -21,7 +21,8 @@ router.get("/", function(request, response){
                    location: activity[i]._activityLocation,
                    date: activity[i]._activityDate,
                    time: activity[i]._activityTime,
-                   description: activity[i]._activityDescription 
+                   description: activity[i]._activityDescription,
+                   datePosted: activity[i]._datePosted
                 }
 
                 packet.push(act)
@@ -36,6 +37,100 @@ router.get("/", function(request, response){
             }
 
             response.render("activities.hbs", model)
+        }
+    })
+})
+
+
+
+router.get("/create", function(request, response){
+    
+    const model = {
+        
+        validationErrors: []
+    }
+
+    response.render("create-activity.hbs", model)
+})
+
+router.post("/create", function(request, response){
+
+    const date = new Date()
+    const datePosted = date.toDateString()
+
+    const activity = {
+        title: request.body.title.trim(),
+        location: request.body.location.trim(),
+        date: request.body.date.trim(),
+        time: request.body.time.trim(),
+        description: request.body.description.trim(),
+        datePosted: datePosted
+    }
+
+    activityManager.createActivity(activity, function(error, activity){
+        if(error){
+            console.log(error)
+            response.render("create-activity.hbs", error, activity)
+        } else{
+            response.redirect("/activities")
+        }
+    })  
+})
+
+router.get("/update/:id", function(request, response){
+
+    const id = request.params.id
+
+    activityManager.getActivityById(id, function(error, activity){
+        if(error){
+            console.log(error)
+            response.render("login.hbs")
+        } else{
+            const model = {
+                activity,
+                id: activity[0]._id,
+                title: activity[0]._activityName,
+                location: activity[0]._activityLocation,
+                date: activity[0]._activityDate,
+                time: activity[0]._activityTime,
+                description: activity[0]._activityDescription,
+            }
+            response.render("update-activity.hbs", model)
+        }
+    })
+})
+
+router.post("/update/:id", function(request, response){
+
+    const activity = {
+        id : request.params.id,
+        title: request.body.title.trim(),
+        location: request.body.location.trim(),
+        date: request.body.date.trim(),
+        time: request.body.time.trim(),
+        description: request.body.description.trim()
+    }
+
+    activityManager.updateActivity(activity, function(error, activity){
+        if(error){
+            console.log(error)
+            response.render("login.hbs")
+        } else {
+            response.redirect("/activities/" + activity[0]._id)
+        }
+    })
+})
+
+router.post("/delete/:id", function(request, response){
+    
+    const id = request.params.id
+
+    activityManager.deleteActivity(id, function(error){
+        if(error){
+            console.log(error)
+            response.render("login.hbs")
+        } else{
+            response.redirect("/activities")
         }
     })
 })
@@ -56,40 +151,12 @@ router.get("/:id", function(request, response){
                 location: activity[0]._activityLocation,
                 date: activity[0]._activityDate,
                 time: activity[0]._activityTime,
-                description: activity[0]._activityDescription
+                description: activity[0]._activityDescription,
+                datePosted: activity[0]._datePosted
             }
             response.render("activity-detailed.hbs", model)
         }
     })
-})
-
-router.get("/create", function(request, response){
-    
-    const model = {
-        
-        validationErrors: []
-    }
-
-    response.render("create-activity.hbs", model)
-})
-
-router.post("/create", function(request, response){
-    const activity = {
-        title: request.body.title.trim(),
-        location: request.body.location.trim(),
-        date: request.body.date.trim(),
-        time: request.body.time.trim(),
-        description: request.body.description.trim()
-    }
-
-    activityManager.createActivity(activity, function(error, activity){
-        if(error){
-            console.log(error)
-            response.render("create-activity.hbs", error, activity)
-        } else{
-            response.redirect("/activities")
-        }
-    })  
 })
 
 module.exports = router
